4e6d249146c8c3ec3910d57680a7024919a83fb4
xubowenjx <286129936@qq.com>
2017-12-01 17:51:18 +0800

    e....

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist/',
-    filename: 'countup.js',
+    filename: 'index.js',
     library: 'countup',
     libraryTarget: 'umd',
     umdNamedDefine: true
